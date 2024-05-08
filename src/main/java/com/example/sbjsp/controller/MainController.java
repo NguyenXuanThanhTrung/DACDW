e14ee21686d5fcabb1ed4496690d389e3abe7862
NguyenXuanThanhTrung <20130452@st.hcmuaf.edu.vn>
2024-05-09 00:21:18 +0700

    mapping for single-blog

--- src/main/java/com/example/sbjsp/controller/MainController.java
+++ src/main/java/com/example/sbjsp/controller/MainController.java
@@ -25,48 +25,63 @@ public class MainController {
         model.addAttribute("contextPath", contextPath);
         return "index";
     }
+
     @RequestMapping(value = {"about"}, method = RequestMethod.GET)
     public String about(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "about";
     }
+
     @RequestMapping(value = {"blog"}, method = RequestMethod.GET)
     public String blog(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "blog";
     }
+
     @RequestMapping(value = {"blog_details"}, method = RequestMethod.GET)
     public String blog_details(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "blog_details";
     }
+
+    @RequestMapping(value = {"single-blog"}, method = RequestMethod.GET)
+    public String singleBlog(Model model) {
+        String contextPath = servletContext.getContextPath();
+        model.addAttribute("contextPath", contextPath);
+        return "single-blog";
+    }
+
     @RequestMapping(value = {"categori"}, method = RequestMethod.GET)
     public String categori(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "category";
     }
+
     @RequestMapping(value = {"contact"}, method = RequestMethod.GET)
     public String contact(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "contact";
     }
+
     @RequestMapping(value = {"elements"}, method = RequestMethod.GET)
     public String elements(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "elements";
     }
+
     @RequestMapping(value = {"latest_news"}, method = RequestMethod.GET)
     public String latest_news(Model model) {
         String contextPath = servletContext.getContextPath();
         model.addAttribute("contextPath", contextPath);
         return "latest_news";
     }
+
     @RequestMapping(value = {"main"}, method = RequestMethod.GET)
     public String main(Model model) {
         String contextPath = servletContext.getContextPath();
